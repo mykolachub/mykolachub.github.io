c7b30aeb9f1aff88d14cdbe4c362d3b61a5be004
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-02-29 22:16:00 +0200

    urklit was changed

---
+++ proj/ukrlit/fonts/ReadMe DF.txt
@@ -0,0 +1,14 @@
+NOTE: This font is for PERSONAL USE ONLY!
+But any donation are very appreciated
+
+Paypal account for donation :
+anytypeco@gmail.com
+
+FOR EXTENDED LICENSE CONTACT ME : anytypeco@gmail.com
+
+Please visit our store for more great fonts:
+https://www.creativefabrica.com/designer/anytypeco/ref/175263/
+
+Follow my Social Media for update : 
+https://www.pinterest.com/AnyTypeCo/
+https://www.instagram.com/anytypeco/
---
+++ proj/ukrlit/js/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('welcome to my console!');
+
+    // DOM variables
+    let menuBtn = document.getElementById('menu-btn'),
+        menuWindow = document.getElementById('menu-modal'),
+        modalBtnToHome = document.getElementById('menu-a-home'),
+        modalBtnToMore = document.getElementById('menu-a-more'),
+        windowBtnToMore = document.getElementById('btn-more'),
+        sectionMore = document.getElementById('fisrt'),
+        modalBtnToAbout = document.getElementById('menu-a-about'),
+        sectionAbout = document.getElementById('about');
+
+
+    // internal variables
+    let currentPos;
+
+    // function describes closing modal `menu` window;
+    function closeModalMenu(params) {
+        menuBtn.classList.toggle('header__menu-active');
+        menuWindow.classList.toggle('menu-active');
+    }
+
+    // function describes the way it moves down to necessary section(target)
+    function scrollToSection(target) { 
+        currentPos = target.getBoundingClientRect().top;
+        console.log('currentPos:',  currentPos);
+            window.scrollTo({
+            top: currentPos,
+            behavior: 'smooth'
+            }); 
+    };
+
+    // function describes the way it moves up to top
+    function moveToTop() { 
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        }); 
+    };
+
+    // typed.js
+    /*var typed = new Typed('#typed-text', {
+        strings: ['Навіщо цей проект?', 'Чому це безкоштовно? '],
+        smartBackspace: true ,
+        loop: true,
+        typeSpeed: 80,
+        backSpeed: 40
+    });*/
+
+    menuBtn.addEventListener('click', ()=>{
+        closeModalMenu();
+    });
+
+    modalBtnToHome.addEventListener('click', () =>{
+        closeModalMenu();
+        moveToTop();
+    });
+
+    modalBtnToMore.addEventListener('click', () =>{
+        closeModalMenu();
+        scrollToSection(sectionMore);
+    });
+
+    windowBtnToMore.addEventListener('click', () =>{
+        scrollToSection(sectionMore);
+    });
+
+
+    modalBtnToAbout.addEventListener('click', () =>{
+        closeModalMenu();
+        scrollToSection(sectionAbout);
+    });
+});
+
