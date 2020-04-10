73f6e2e95adcd465113651e1f42fd7cb86680e78
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-04-10 18:37:22 +0300

    eUkraine font was added

--- proj/ukrlit/js/index.js
+++ proj/ukrlit/js/index.js
@@ -4,28 +4,31 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log('welcome to my console!');
 
     // DOM variables
-    let menuBtn = document.getElementById('menu-btn'),
-        menuWindow = document.getElementById('menu-modal'),
-        modalBtnToHome = document.getElementById('menu-a-home'),
-        modalBtnToMore = document.getElementById('menu-a-more'),
-        modalMenuItems = document.querySelectorAll('.menu__item'),
-        windowBtnToMore = document.getElementById('btn-more'),
-        sectionMore = document.getElementById('fisrt'),
-        modalBtnToAbout = document.getElementById('menu-a-about'),
-        sectionAbout = document.getElementById('about');
+    let section1 = document.getElementById('section-1'),
+        wrapper = document.getElementById('wrapper');
 
-    let btnStart = document.getElementById('menu-a-start'),
-        btnStart2 = document.getElementById('btn-start'),
-        footerBtnStart = document.getElementById('footer-btn-start');
+    let scrolled,
+        position;
 
     // internal variables
     let currentPos;
 
-    // function describes closing modal `menu` window;
-    function closeModalMenu(params) {
-        menuBtn.classList.toggle('header__menu-active');
-        menuWindow.classList.toggle('menu-active');
-    }
+    position = section1.getBoundingClientRect().top;
+    console.log('position: ', position);
+
+    window.addEventListener('scroll', ()=>{
+        scrolled = window.pageYOffset;
+        position = section1.getBoundingClientRect().top;
+        if (scrolled < position) {
+            console.log('<');
+            wrapper.classList.remove('change-color-pink');
+        }
+        else{
+            console.log('=!');
+            wrapper.classList.add('change-color-pink');
+        }
+
+    });
 
     // function describes the way it moves down to necessary section(target)
     function scrollToSection(target) { 
@@ -46,56 +49,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     };
 
     // wow.js
-    new WOW().init();
-
-
-    //
-
-
-    // func changes menu item name
-    if(window.matchMedia('(max-width: 560px)').matches){
-       for (let i = 0; i < modalMenuItems.length; i++) {
-            modalMenuItems[i].childNodes[0].data = ''; 
-            const modalMenuItemsLib = ['Головна', "Про проект", "Про команду", "Розпочати"];
-            modalMenuItems[i].childNodes[1].innerHTML = `${modalMenuItemsLib[i]}`;
-           console.log(modalMenuItems[i].childNodes[1]);
-       }
-    }
-
-    menuBtn.addEventListener('click', ()=>{
-        closeModalMenu();
-    });
-
-    modalBtnToHome.addEventListener('click', () =>{
-        closeModalMenu();
-        moveToTop();
-    });
-
-    modalBtnToMore.addEventListener('click', () =>{
-        closeModalMenu();
-        scrollToSection(sectionMore);
-    });
-
-    windowBtnToMore.addEventListener('click', () =>{
-        scrollToSection(sectionMore);
-    });
-
-
-    modalBtnToAbout.addEventListener('click', () =>{
-        closeModalMenu();
-        scrollToSection(sectionAbout);
-    });
-
-    btnStart.addEventListener('click', () =>{
-        window.open('main.html')
-    });
-
-    btnStart2.addEventListener('click', () =>{
-        window.open('main.html')
-    });
-
-    footerBtnStart.addEventListener('click', () =>{
-        window.open('main.html')
-    });
+    //new WOW().init();
 });
 
