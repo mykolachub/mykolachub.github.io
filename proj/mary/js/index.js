6729d44d5ff758e4714c1cd763913689c23b4532
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-05-27 00:08:58 +0300

    'mary' undated design

--- proj/mary/js/index.js
+++ proj/mary/js/index.js
@@ -2,7 +2,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
     console.log('welcome to my console!');
 
-    btnToTop.addEventListener('click' , moveToTop); // btn of moving to top
+    let starterTime = document.getElementById('starterTime'),
+        firstDevider = document.getElementById('firstDevider'),
+        wrapper = document.getElementById('wrapper'),
+        letter = document.getElementById('letter'),
+        marquee = document.getElementById('marquee');
 
     function moveToTop() { // func describes the way it moves up to top
         window.scrollTo({
@@ -11,12 +15,57 @@ document.addEventListener('DOMContentLoaded', () =>{
         }); 
     };
 
+    console.log(wrapper.getBoundingClientRect());
+
+    const interval = setInterval(() => {
+        let CurrentTime = new Date();
+        starterTime.innerHTML = CurrentTime.getHours()+ ':'+CurrentTime.getMinutes()+':'+CurrentTime.getSeconds();
+    }, 1000);
+
     function scrollToSection(target) { //func describes the way it moves down to necessary section(target)
         currentPos = target.getBoundingClientRect().top;
         console.log('currentPos:',  currentPos);
             window.scrollTo({
             top: currentPos,
             behavior: 'smooth'
-            }); 
+        }); 
     };
+
+    window.addEventListener('scroll', ()=>{
+        let scrolled = window.pageYOffset;
+        let position1 = firstDevider.getBoundingClientRect().top * 2;
+        //position2 = section2.getBoundingClientRect().top;
+        
+        if (scrolled <= position1) {
+            wrapper.classList.remove('works-focus');
+        }
+        else{
+            wrapper.classList.add('works-focus');
+        }
+
+    });
+
+    let workImg = document.querySelectorAll('.photo-container .photo');
+
+    let randomIndex;
+    workImg.forEach(element => {
+        function getRandomArbitary(min, max) {
+            randomIndex = Math.random() * (max - min) + min;
+        }
+        getRandomArbitary(1 , 4);
+        element.setAttribute('style', `mask-image: url(./imgs/icons/blob${Math.round(randomIndex)}.svg);`);
+        console.log(element);
+    });
+
+
+    /*function tickerWidth() {
+        marquee.setAttribute('style', `width: ${letter.offsetWidth}px !important;`);
+    }
+
+    tickerWidth();
+
+    window.addEventListener('resize', ()=>{
+        tickerWidth();
+    });*/
+
 });
