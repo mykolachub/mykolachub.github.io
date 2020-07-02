e375211e2eb1955f163eed69e26756c5b8eafc9a
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-07-03 00:36:54 +0300

    'natish' about section was added

--- proj/natish/js/index.js
+++ proj/natish/js/index.js
@@ -1,1 +1,25 @@
-console.log('kok');
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () =>{
+    console.log('welcome to my console');
+
+    let aboutQuiz = document.getElementById('aboutQuiz'),
+        aboutQuizNo = document.getElementById('aboutQuizNo'),
+        aboutQuizYes = document.getElementById('aboutQuizYes'),
+        aboutQuizReply = document.getElementById('aboutQuizReply');
+        aboutQuizReplyText = document.getElementById('aboutQuizReplyText');
+    
+
+    aboutQuizYes.addEventListener('click', ()=>{
+        aboutQuiz.classList.add('quiz__question-off');
+        aboutQuizReplyText.innerHTML = `it's nice to have you. Let me show you other works, then`;
+        aboutQuizReply.classList.add('quiz__reply-on');
+    });
+
+    aboutQuizNo.addEventListener('click', ()=>{
+        aboutQuiz.classList.add('quiz__question-off');
+        aboutQuizReplyText.innerHTML = 'Then, let me show you other works to change your mind, tough guy';
+        aboutQuizReply.classList.add('quiz__reply-on');
+    });
+
+});
