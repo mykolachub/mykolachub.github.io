c8f21639fb0879bc0190c0099d1b15be38f614b8
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-07-06 22:07:59 +0300

    'natish' fonts and work section were created

--- proj/natish/js/index.js
+++ proj/natish/js/index.js
@@ -3,23 +3,47 @@
 document.addEventListener('DOMContentLoaded', () =>{
     console.log('welcome to my console');
 
+    let body = document.getElementById('body');
+
+    // main section
+    let mainContent = document.getElementById('mainContent'),
+        headerNavLast = document.getElementById('headerNavLast');
+
+    // about section
     let aboutQuiz = document.getElementById('aboutQuiz'),
         aboutQuizNo = document.getElementById('aboutQuizNo'),
         aboutQuizYes = document.getElementById('aboutQuizYes'),
-        aboutQuizReply = document.getElementById('aboutQuizReply');
-        aboutQuizReplyText = document.getElementById('aboutQuizReplyText');
+        aboutQuizReply = document.getElementById('aboutQuizReply'),
+        aboutQuizReplyText = document.getElementById('aboutQuizReplyText'),
+        aboutQuizLoading = document.getElementById('aboutQuizLoading');
     
+    // work section
+    let workWrapper = document.getElementById('workWrapper'),
+        workMask = document.getElementById('workMask'),
+        workContent = document.getElementById('workContent');
+
+
 
     aboutQuizYes.addEventListener('click', ()=>{
-        aboutQuiz.classList.add('quiz__question-off');
         aboutQuizReplyText.innerHTML = `it's nice to have you. Let me show you other works, then`;
-        aboutQuizReply.classList.add('quiz__reply-on');
+        workOpenQUIZ();
     });
 
     aboutQuizNo.addEventListener('click', ()=>{
-        aboutQuiz.classList.add('quiz__question-off');
         aboutQuizReplyText.innerHTML = 'Then, let me show you other works to change your mind, tough guy';
-        aboutQuizReply.classList.add('quiz__reply-on');
+        workOpenQUIZ();
     });
+
+    function workOpenQUIZ() {
+        aboutQuiz.classList.add('quiz__question-off');
+        aboutQuizLoading.innerHTML = 'wait';
+        aboutQuizReply.classList.add('quiz__reply-on');
+        aboutQuizLoading.classList.add('quiz__loading-on');
+        workMask.classList.add('work__mask-on');
+        workWrapper.classList.add('work__wrapper-on');
+        body.classList.add('body-off');
+        headerNavLast.classList.add('header__nav-last');
+        workContent.classList.add('work__content-on');
+    }
 
 });
