b1c7643f696fd49c587f569d4f5a061d1a2a24ba
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-07-23 16:29:51 +0300

    'allquasars' signup page was finiSHED

--- proj/quazistellar/js/singup.js
+++ proj/quazistellar/js/singup.js
@@ -47,222 +47,73 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
     });
 
-    nameWrapper.addEventListener('click', ()=>{
-        if (nameWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            nameWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputName.addEventListener('focus', ()=>{
-        if (nameWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            nameWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(nameWrapper);
     });
 
     inputName.addEventListener('blur', ()=>{
-        if (nameWrapper.classList.contains('form__clue--clicked') == true) {
-            nameWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(nameWrapper);
     });
 
-    surnameWrapper.addEventListener('click', ()=>{
-        if (surnameWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            surnameWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputSurname.addEventListener('focus', ()=>{
-        if (surnameWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            surnameWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(surnameWrapper);
     });
 
     inputSurname.addEventListener('blur', ()=>{
-        if (surnameWrapper.classList.contains('form__clue--clicked') == true) {
-            surnameWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(surnameWrapper);
     });
 
-
-    dateWrapper.addEventListener('click', ()=>{
-        if (dateWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            dateWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputDate.addEventListener('focus', ()=>{
-        if (dateWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            dateWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(dateWrapper);
     });
 
     inputDate.addEventListener('blur', ()=>{
-        if (dateWrapper.classList.contains('form__clue--clicked') == true) {
-            dateWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(dateWrapper);
     });
 
-
-    mailWrapper.addEventListener('click', ()=>{
-        if (mailWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            mailWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputMail.addEventListener('focus', ()=>{
-        if (mailWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            mailWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(mailWrapper);
     });
 
     inputMail.addEventListener('blur', ()=>{
-        if (mailWrapper.classList.contains('form__clue--clicked') == true) {
-            mailWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(mailWrapper);
     });
 
-    passwordWrapper.addEventListener('click', ()=>{
-        if (passwordWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            passwordWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputPassword.addEventListener('focus', ()=>{
-        if (passwordWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            passwordWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(passwordWrapper);
     });
 
     inputPassword.addEventListener('blur', ()=>{
-        if (passwordWrapper.classList.contains('form__clue--clicked') == true) {
-            passwordWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(passwordWrapper);
     });
 
-    passwordcheckWrapper.addEventListener('click', ()=>{
-        if (passwordcheckWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            passwordcheckWrapper.classList.toggle('form__clue--clicked');
-        }
-    });
-
+    //
     inputPassworCheck.addEventListener('focus', ()=>{
-        if (passwordcheckWrapper.classList.contains('form__clue--clicked') == true) {
-            return;
-        } else {
-            passwordcheckWrapper.classList.toggle('form__clue--clicked');
-        }
+        inputFocused(passwordcheckWrapper);
     });
 
     inputPassworCheck.addEventListener('blur', ()=>{
-        if (passwordcheckWrapper.classList.contains('form__clue--clicked') == true) {
-            passwordcheckWrapper.classList.toggle('form__clue--clicked');
-            
-        } else {
-            return;
-        }
+        inputBlured(passwordcheckWrapper);
     });
 
-    document.addEventListener('click', e=>{
-        if (inputName.value == '') { // если ничего не ввел в инпут
-            if (event.clientX >= nameWrapper.getBoundingClientRect().left && event.clientX <= nameWrapper.getBoundingClientRect().right && event.clientY >= nameWrapper.getBoundingClientRect().top && event.clientY <= nameWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                nameWrapper.classList.remove('form__clue--clicked');
-            }
-        } else {
-            return;
+    // main functions
+    function inputFocused(e) {
+        if (!e.classList.contains('form__clue--clicked')) {
+            e.classList.add('form__clue--clicked');
+            console.log('focused');
         }
+    };
 
-        if(inputSurname.value == ''){ // если ничего не ввел в инпут
-            if (event.clientX >= surnameWrapper.getBoundingClientRect().left && event.clientX <= surnameWrapper.getBoundingClientRect().right && event.clientY >= surnameWrapper.getBoundingClientRect().top && event.clientY <= surnameWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                surnameWrapper.classList.remove('form__clue--clicked');
-            }
-        } else{
-            return;
+    function inputBlured(e) {
+        if (e.classList.contains('form__clue--clicked') && e.firstElementChild.value == '') {
+            e.classList.remove('form__clue--clicked'); 
+            console.log('blured');
         }
-
-        if(inputMail.value == ''){ // если ничего не ввел в инпут
-            if (event.clientX >= mailWrapper.getBoundingClientRect().left && event.clientX <= mailWrapper.getBoundingClientRect().right && event.clientY >= mailWrapper.getBoundingClientRect().top && event.clientY <= mailWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                mailWrapper.classList.remove('form__clue--clicked');
-            }
-        } else{
-            return;
-        }
-
-        
-
-        if(inputDate.value == ''){ // если ничего не ввел в инпут
-            if (event.clientX >= dateWrapper.getBoundingClientRect().left && event.clientX <= dateWrapper.getBoundingClientRect().right && event.clientY >= dateWrapper.getBoundingClientRect().top && event.clientY <= dateWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                dateWrapper.classList.remove('form__clue--clicked');
-            }
-        } else{
-            return;
-        }
-
-        if(inputPassword.value == ''){ // если ничего не ввел в инпут
-            if (event.clientX >= passwordWrapper.getBoundingClientRect().left && event.clientX <= passwordWrapper.getBoundingClientRect().right && event.clientY >= passwordWrapper.getBoundingClientRect().top && event.clientY <= passwordWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                passwordWrapper.classList.remove('form__clue--clicked');
-            }
-        } else{
-            return;
-        }
-
-        if(inputPassworCheck.value == ''){ // если ничего не ввел в инпут
-            if (event.clientX >= passwordcheckWrapper.getBoundingClientRect().left && event.clientX <= passwordcheckWrapper.getBoundingClientRect().right && event.clientY >= passwordcheckWrapper.getBoundingClientRect().top && event.clientY <= passwordcheckWrapper.getBoundingClientRect().bottom) {
-                return;
-            } else{ // клик все инпута отключает анимацию
-                passwordcheckWrapper.classList.remove('form__clue--clicked');
-            }
-        } else{
-            return;
-        }
-        
-    });
-
-
+        //console.log(e.firstElementChild.value);
+    };
 });
